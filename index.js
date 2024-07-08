84a910b21ba3dee6fa03cf01b5bbf94937a71ba4
NOMI9797 <139885182+NOMI9797@users.noreply.github.com>
2024-07-08 09:45:27 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// // const mysym=Symbol["key1"];
+// // const nomi={
+// //     name:"noman",
+// //     age:20,
+// //     islogged:true,
+// //     [mysym]:"mykey1"
+// // };
+// // console.log(nomi["name"]);
+// // console.log(nomi[mysym]);
+// // nomi.greeting=function(){
+// //     console.log('hello js user',this.name);
+// // }
+// // console.log(nomi.greeting());
 
+// // function addtwo(...num1) {
+// //     return num1;
+// // }
+
+// // console.log(addtwo(3, 4, 5, 6, 7, 8));
+// ((name) => {
+//     console.log(`Welcome ${name}`);
+// })('nomi');
+const descriptor=Object.getOwnPropertyDescriptor(Math,"PI");
+console.log(descriptor);
